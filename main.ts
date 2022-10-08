32e94fca2afa81e72d0f373dc291d88a9bbdc9e3
hrbqian <115328381+hrbqian@users.noreply.github.com>
2022-10-08 09:25:38 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+    for (let Y = 0; Y <= 4; Y++) {
+        for (let X = 0; X <= 4; X++) {
+            led.plot(X, Y)
+            basic.pause(200)
+            led.unplot(X, Y)
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let X = 0; X <= 4; X++) {
+        for (let Y = 0; Y <= 4; Y++) {
+            led.plot(X, Y)
+            basic.pause(200)
+            led.unplot(X, Y)
+        }
+    }
+})
+let index = 0
+basic.forever(function () {
+	
+})
