1967d9dadd7a71e95848f94d473578b76fee5155
hrbqian <115328381+hrbqian@users.noreply.github.com>
2022-10-08 09:46:33 +0800

    文件名

--- main.ts
+++ main.ts
@@ -7,16 +7,17 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.B, function () {
     for (let X = 0; X <= 4; X++) {
         for (let Y = 0; Y <= 4; Y++) {
             led.plot(X, Y)
             basic.pause(200)
-            led.unplot(X, Y)
         }
     }
 })
-let index = 0
 basic.forever(function () {
 	
 })
